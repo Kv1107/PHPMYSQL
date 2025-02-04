a1c1fdb061b2d6ffdd6f2236d76035cc67861b51
Kv1107 <115678358+Kv1107@users.noreply.github.com>
2025-02-03 17:35:09 -0700

    Create predefined.php

---
+++ predefined.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8>
+	<title> Predefined Variables</title>
+</head>
+<body>
+<prep>
+<?php // Script 2.1 oredefined.php
+
+//Show the calue of $_SERVER Variable
+print_r($_SERVER);
+
+?>
+</prep>
+</body>
+</html>

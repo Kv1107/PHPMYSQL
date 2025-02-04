1df09452b3fb89ddb280649d2d24ab590349f046
Kv1107 <115678358+Kv1107@users.noreply.github.com>
2025-02-03 17:34:45 -0700

    Create commentlineexamples.php

---
+++ commentlineexamples.php
@@ -0,0 +1,8 @@
+<?php
+
+	echo "Hello!!";
+// This is an example of a comment on a single line
+/* I can do a multiline
+comment with this symbol and it will continue
+until the line is closed */
+?>

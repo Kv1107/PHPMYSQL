78ed6668174002234669a335bb32f719da7f4e86
Kv1107 <115678358+Kv1107@users.noreply.github.com>
2025-02-04 19:59:55 -0700

    Create variables.php

---
+++ variables.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Variable Examples</h1>
+
+<?PHP
+//numbers examples
+$miles = 36; 
+$totalmileage = 155600;
+
+//string Examples
+$intro = "The current cars I am driving are: ";
+$subaru = "My favorite car is my subaru";
+
+// array Examples
+$cars = array ("subaru impreza", "tesla model 3", "dodge ram");
+$future = array ("dodge challenger", "tesla model y");
+
+print "<p> <br> $intro $cars <br> $subaru . The subaru goes $miles per gallon and has $totalmileage. In the future I hope to get $future. </p>";
+?>
+
+</body>
+</html>

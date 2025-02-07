2e6f774515375fe3e05bfd9d75f3fed6eafddf3e
Kv1107 <115678358+Kv1107@users.noreply.github.com>
2025-02-07 13:27:14 -0700

    Create variables.php

---
+++ variables.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title> Variables</title>
+</head>
+<body>
+<?php // Script 2.3 - variables.php
+
+//Address
+$street = "1754 Long Rifle Rd";
+$city = "Camp Verde";
+$state = "AZ";
+$zip = 86322;
+$name = "Kavindra Blount";
+$phone = 9284732518;
+
+
+//Print info
+
+print "<p> My name is $name and I live at : <br> $street 
+<br> $city $state $zip. <br> You can reach me on my cell phone at $phone.";
+
+?>
+</body>
+</html>

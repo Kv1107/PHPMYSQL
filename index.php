8e7bfc0418c28393f20be88459278ba0cf086e25
Kv1107 <115678358+Kv1107@users.noreply.github.com>
2025-02-25 21:41:25 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php // Script 8.4 - index.php
+
+// Include the header:
+include('templates/header.html');
+// Leave the PHP section to display lots of HTML:
+?>
+
+<h2>The Text is From a Random Paragraph Generator</h2>
+<p> He couldn't remember exactly where he had read it, but he was sure that he had. The fact that she didn't believe him was quite frustrating as he began to search the Internet to find the article. It wasn't as if it was something that seemed impossible. Yet she insisted on always seeing the source whenever he stated a fact. </P>
+
+<?php // Return to PHP.
+include('templates/footer.html'); 
+?>
+
+<?php //Script 8.4 - index.php

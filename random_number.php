ad55d310c8d52b7e7299e34541211eb5fbff1a1e
Kv1107 <115678358+Kv1107@users.noreply.github.com>
2025-02-25 17:08:52 -0700

    Create random_number.php

---
+++ random_number.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang = "en">
+<head>
+	<meta charset="utf-8">
+	<title> Random Number Test</title>
+</head>
+<body>
+
+<?php
+print "<p> Here is a random number between 10 and 100: <br> </p>";
+echo(rand(10,100));
+?>
+</body>
+</html>

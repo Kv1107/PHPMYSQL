4262863eecfaef0166ad8b6df6d17ee168b16ac0
Kv1107 <115678358+Kv1107@users.noreply.github.com>
2025-02-25 19:56:52 -0700

    Create soups1.php

---
+++ soups1.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title> No Soup for You! </title>
+</head>
+<body>
+<h1> Mmmmmm....soups</h1>
+<?php // Script 7.1 - soups1.php
+
+//Create the array:
+$soups = [
+'Monday' => 'Tomato Basil Bisque',
+'Tuesday' => 'Paris Bean Bistro',
+'Wednesday' => 'Wild Rice Mushroom',
+'Thursday' => 'Autumn Squash Apple Bisque',
+'Friday' => 'Clam Chowder',
+'Saturday' => 'Tortilla',
+'Sunday' => 'Vegan Chili' ];
+
+//Print array:
+print "<p>$soups</p>";
+
+//Print the contents of the array:
+print_r($soups);
+
+?>
+</body>
+</html>

29e13f1bec45fc0a4eb6f5514b84ec566e0ee620
Kv1107 <115678358+Kv1107@users.noreply.github.com>
2025-03-12 02:33:26 -0700

    Create view_entries.php

---
+++ view_entries.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title> View My Blog </title>
+</head>
+<body>
+<h1>My Blog</h1>
+<?php //Script 12.6 view_entries.php
+
+//Connect and select
+$dbc = mysqli_connect ('localhost', 'root', 'newpassword', 'myblog');
+
+//Define the query:
+$query = 'SELECT * FROM entries ORDER BY date_entered DESC';
+
+if ($r = mysqli_query($dbc, $query)) { //Run the query
+
+//Retrieve and print every record
+while ($row = mysqli_fetch_array($r)) {
+	print "<p><h3>{$row['title']}</h3>
+		{$row['entry']}<br>
+		<a href=\"edit_entry.php?id={$row['id']}\">Edit</a>
+		<a href=\"delete_entry.php?id={$row['id']}\">Delete</a>
+		</[><hr>\n";
+}
+} else { //Query didn't Run
+	print '<p style="color: red;">Could not retrieve the data because:<br>' . mysqli_error($dbc). ' .</p><p>The query being run was: ' . $query . '</p>';
+}//End of query IF
+
+mysqli_close($dbc); //Close the connection
+
+?>
+</body>
+</html>
